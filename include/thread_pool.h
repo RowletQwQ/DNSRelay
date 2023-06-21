85bdb84d60a8a90b65c4a51e97df48fd28908016
Rowlet <2474095154@qq.com>
2023-06-22 01:45:46 +0800

    更改线程池相关函数

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -7,9 +7,9 @@
 #include <process.h>
 
 typedef HANDLE thread_t;
-typedef HANDLE mutex_t;
-typedef HANDLE cond_t;
-typedef HANDLE sem_t;
+typedef CRITICAL_SECTION mutex_t;
+typedef CONDITION_VARIABLE cond_t;
+typedef HANDLE my_sem_t;
 
 #else
 #include <pthread.h>
@@ -18,15 +18,15 @@ typedef HANDLE sem_t;
 typedef pthread_t thread_t;
 typedef pthread_mutex_t mutex_t;
 typedef pthread_cond_t cond_t;
-typedef sem_t sem_t;
+typedef sem_t my_sem_t;
 
 #endif
 //线程池
 struct thread_pool_t{
     thread_t *threads; //线程数组
     int thread_count; //线程数量
-    sem_t tasks; //任务信号量,表示任务数量
-    sem_t spaces; //空闲线程信号量,表示空闲线程数量
+    my_sem_t tasks; //任务信号量,表示任务数量
+    my_sem_t spaces; //空闲线程信号量,表示空闲线程数量
     mutex_t tasks_mutex; //任务互斥锁
     cond_t tasks_cond; //任务条件变量
     int front; //任务队列头
@@ -34,7 +34,7 @@ struct thread_pool_t{
     int tasks_count; //任务数量
     int shutdown; //线程池是否关闭
     void (*process)(void *arg); //任务处理函数
-    void **arg; //任务参数
+    void **task_arg; //任务参数
 };
 typedef struct thread_pool_t thread_pool_t;
 
