bd3988634d3623a0f00b4668e8eac2928259a5e6
Rowlet <2474095154@qq.com>
2023-06-24 11:29:03 +0800

    更改时间格式

--- src/common/logger.c
+++ src/common/logger.c
@@ -84,7 +84,7 @@ const char* get_time(){
         printf("logger:malloc failed\n");
         exit(1);
     }
-    sprintf(time_str, "%d-%d-%d %02d:%02d:%02d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday,
+    sprintf(time_str, "%d-%02d-%02d %02d:%02d:%02d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday,
         local->tm_hour, local->tm_min, local->tm_sec);
     return time_str;
 }
